49e17f9c46128a4cd47dfda245c5380b0e45d64b
anud18 <as12345033@gmail.com>
2023-05-17 23:46:46 +0800

    PR_test

---
+++ hw4/test/dijkstra/fix_x.cpp
@@ -0,0 +1,155 @@
+// Program to find Dijkstra's shortest path using
+// priority_queue in STL
+#include <bits/stdc++.h>
+#include <boost/heap/fibonacci_heap.hpp>
+//#include <boost/heap/fibonacci_heap.hpp>
+
+using namespace boost::heap;
+using namespace std;
+
+typedef pair<int, int> PII;
+typedef boost::heap::fibonacci_heap<
+PII,
+    boost::heap::compare<std::greater<PII> > > MyHeap;
+using Heap = boost::heap::fibonacci_heap<
+PII,
+    boost::heap::compare<std::greater<PII> > >;
+
+const int nVecS = 1000;
+class Graph{
+    public:
+        Graph(){n = 0;}
+        vector<PII> adj[1000];
+    private:
+        int n;
+};
+const int z = 300;
+#define INF 0x3f3f3f3f
+int sum = 0;
+unsigned int seed = 1 + 100*1;
+mt19937 gen(seed);
+uniform_int_distribution<> r_val(0,999);
+
+
+
+// To add an edge
+void addEdge(vector<PII> adj[], int u, int v,
+        int wt)
+{
+    adj[u].push_back(make_pair(v, wt));
+    adj[v].push_back(make_pair(u, wt));
+}
+
+// Prints shortest paths from src to all other vertices
+void shortestPath(vector<PII> adj[nVecS], int V,
+        int src, int target = -1)
+{
+    MyHeap	pq;
+
+    vector<int> dist(V, INF);
+
+
+    MyHeap::handle_type h[V];
+    h[src] = pq.push(make_pair(0, src));
+    dist[src] = 0;
+    vector<bool> vis(V, false);
+    //vector<int> vis(n, false);
+
+    /* Looping till priority queue becomes empty (or all
+       distances are not finalized) */
+    while (!pq.empty()) {
+        int u = pq.top().second;
+        int k = pq.top().first;
+        if(target != -1 && u == target){
+            //cout << u << ' ' << dist[u] << endl;
+            sum += dist[target];
+            return;
+
+
+        }
+
+        pq.pop();
+
+        // Get all adjacent of u.
+        for (auto x : adj[u]) {
+            // Get vertex label and weight of current
+            // adjacent of u.
+
+            // If there is shorted path to v through u.
+            // Updating distance of v
+            int v = x.first;
+            int weight = x.second;
+
+            // If there is shorted path to v through u.
+            if (dist[v] > dist[u] + weight) {
+                // Updating distance of v
+                dist[v] = dist[u] + weight;
+                if(dist[v] == INF){
+                    pq.increase(h[v], make_pair(dist[v], v));
+                }
+                else
+                    pq.push(make_pair(dist[v], v));
+            }
+        }
+    }
+
+    // Print shortest distances stored in dist[]
+    //printf("Vertex Distance from Source\n");
+    for (int i = 0; i < V; ++i)
+        sum += dist[i];
+    //if(target >= 0)
+        //sum += dist[target];
+}
+template<typename T , std::size_t N>
+void add_x_edges(T (&adj)[N], int x, int wt){
+
+    for (int times = 0; times < x; ++times) {
+        int u = r_val(gen);
+        int v = r_val(gen);
+        bool tag = true;
+        while(tag){
+            tag = false;
+            for(auto it : adj[u]){
+                if ( u == v || it.first == v) {
+                    u = r_val(gen);
+                    v = r_val(gen);
+                    tag = true;
+                }
+            }
+        }
+        addEdge(adj, u, v, wt);
+    }
+
+}
+
+bool sampleFind(vector<PII> &sset, int a, int b){
+    if(a == b)
+        return true;
+    PII tmp(a, b);
+    PII _tmp(b, a);
+    for(auto it : sset){
+        if(it == tmp || it == _tmp)
+            return true;
+    }
+    return false;
+
+}
+
+// Driver program to test methods of graph class
+int main()
+{
+     const int x = 775;
+    for(int y = 0; y < 1500; ++y){
+        Graph G;
+        for(int i = 0; i < nVecS; ++i){
+            addEdge(G.adj, i % 1000 , (i + 1) % 1000, 1);
+        }
+        sum = 0;
+        add_x_edges(G.adj, x, y);
+        for(int i = 0; i < nVecS; ++i)
+            shortestPath(G.adj, nVecS, i);
+        float out = (float) sum / (1000*999/2);
+        cout <<  y << ' ' << out << endl;
+    }
+    return 0;
+}
